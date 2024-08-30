613989da05a28d6c185f0b1d2cd27619fc7d0c1b
fazal <fazalahamed628@gmail.com>
2024-08-30 12:43:14 +0530

    fix:fetch clans issue

--- clan-service/src/application/interfaces/IDependencies.ts
+++ clan-service/src/application/interfaces/IDependencies.ts
@@ -1,9 +1,9 @@
-import { IUseCases } from './IUseCases'
+
 import {IRepositories} from '@/application/interfaces/IRepositories'
 
 
 
 export interface  IDependencies{
     repositories:IRepositories,
-    useCases:IUseCases
+  
 }
