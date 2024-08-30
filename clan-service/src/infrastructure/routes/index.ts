aab72f37fb82daa9636b707c12cdc18d6a84b1c1
fazal <fazalahamed628@gmail.com>
2024-08-30 12:28:51 +0530

    fix:clans issue

--- clan-service/src/infrastructure/routes/index.ts
+++ clan-service/src/infrastructure/routes/index.ts
@@ -4,13 +4,13 @@ import { IDependencies } from "@/application/interfaces/IDependencies";
 
 
 export const routes = (dependencies:IDependencies) => {
-  const{createClan,fetchAllClans,fetchAllMember,fetchAllUsers,addMember,deleteMember,blockClan,leaveClan,requestClan,acceptClan,completeQuiz,rejectClan}=controller(dependencies)
+  const{createClan,fetchAllMember,fetchAllUsers,addMember,deleteMember,blockClan,leaveClan,requestClan,acceptClan,completeQuiz,rejectClan}=controller(dependencies)
  
 
   const router = Router();
 
   router.route('/create-clan').post(createClan)
-  router.route('/fetch-all-clans').get(fetchAllClans)
+ 
   router.route('/fetchmember').post(fetchAllMember)
   router.route('/fetch-all-users').get(fetchAllUsers)
   router.route('/add-member').post(addMember)
